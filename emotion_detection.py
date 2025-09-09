369ff97a66aa81d7ea2749e7c76d4fe9c3e86b18
SADAGRUB <savadogo5@live.com>
2025-09-09 02:53:06 -0400

    version avec tache trois

--- emotion_detection.py
+++ emotion_detection.py
@@ -4,13 +4,31 @@ using IBM Watson's pre-trained EmotionPredict service.
 """
 
 import requests
-
+import json
 
 def emotion_detector(text_to_analyse):
-   
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = {"raw_document": {"text": text_to_analyse}}
+
+    # Requête API
     response = requests.post(url, json=myobj, headers=headers)
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotion_dic = formatted_response["emotionPredictions"][0]["emotion"]
+
+    anger_score = emotion_dic["anger"]
+    disgust_score = emotion_dic["disgust"]
+    fear_score = emotion_dic["fear"]
+    joy_score = emotion_dic["joy"]
+    sadness_score = emotion_dic["sadness"]
+    dominant_emotion = max(emotion_dic, key=emotion_dic.get)
+
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion
+    }
